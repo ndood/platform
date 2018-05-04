850c995aab9902ca8e7153998085de18b901fbcd
yanbiao38156 <yanbiao38156@126.com>
2018-05-04 17:22:26 +0800

    内容管理手续费查询bug

--- game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/CategoryServiceImpl.java
+++ game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/CategoryServiceImpl.java
@@ -18,6 +18,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -26,12 +27,11 @@ import com.fulu.game.core.entity.Category;
 import com.fulu.game.core.service.CategoryService;
 
 
-
 @Service
-public class CategoryServiceImpl extends AbsCommonService<Category,Integer> implements CategoryService {
+public class CategoryServiceImpl extends AbsCommonService<Category, Integer> implements CategoryService {
 
     @Autowired
-	private CategoryDao categoryDao;
+    private CategoryDao categoryDao;
     @Autowired
     private TechAttrService techAttrService;
     @Autowired
@@ -46,18 +46,18 @@ public class CategoryServiceImpl extends AbsCommonService<Category,Integer> impl
 
     @Override
     public PageInfo<Category> list(int pageNum, int pageSize) {
-        return list(pageNum,pageSize,true,null);
+        return list(pageNum, pageSize, true, null);
     }
 
     @Override
-    public PageInfo<Category> list(int pageNum, int pageSize, Boolean status,String orderBy) {
+    public PageInfo<Category> list(int pageNum, int pageSize, Boolean status, String orderBy) {
         CategoryVO categoryVO = new CategoryVO();
         categoryVO.setStatus(status);
         categoryVO.setPid(CategoryParentEnum.ACCOMPANY_PLAY.getType());
-        if(StringUtils.isNotBlank(orderBy)){
+        if (StringUtils.isNotBlank(orderBy)) {
             orderBy = "sort desc";
         }
-        PageHelper.startPage(pageNum,pageSize,orderBy);
+        PageHelper.startPage(pageNum, pageSize, orderBy);
         List<Category> categoryList = categoryDao.findByParameter(categoryVO);
         PageInfo page = new PageInfo(categoryList);
         return page;
@@ -67,23 +67,24 @@ public class CategoryServiceImpl extends AbsCommonService<Category,Integer> impl
     @Override
     public CategoryVO findCategoryVoById(Integer id) {
         Category category = categoryDao.findById(id);
+        category.setCharges(category.getCharges().multiply(new BigDecimal(100)));
         CategoryVO categoryVO = new CategoryVO();
-        BeanUtil.copyProperties(category,categoryVO);
+        BeanUtil.copyProperties(category, categoryVO);
 
         //查询销售方式和段位
         List<TechAttr> techAttrList = techAttrService.findByCategory(category.getId());
-        for(TechAttr techAttr : techAttrList){
-            if(TechAttrTypeEnum.SALES_MODE.getType().equals(techAttr.getType())){
-                List<TechValue>  salesModeList =techValueService.findByTechAttrId(techAttr.getId());
+        for (TechAttr techAttr : techAttrList) {
+            if (TechAttrTypeEnum.SALES_MODE.getType().equals(techAttr.getType())) {
+                List<TechValue> salesModeList = techValueService.findByTechAttrId(techAttr.getId());
                 categoryVO.setSalesModeList(salesModeList);
             }
-            if(TechAttrTypeEnum.DAN.getType().equals(techAttr.getType())){
+            if (TechAttrTypeEnum.DAN.getType().equals(techAttr.getType())) {
                 List<TechValue> danList = techValueService.findByTechAttrId(techAttr.getId());
                 categoryVO.setDanList(danList);
             }
         }
         //查询游戏标签
-        if(category.getTagId()!=null){
+        if (category.getTagId() != null) {
             List<Tag> tagList = tagService.findByPid(category.getTagId());
             categoryVO.setTagList(tagList);
         }
@@ -92,17 +93,17 @@ public class CategoryServiceImpl extends AbsCommonService<Category,Integer> impl
 
 
     @Override
-    public List<Category> findAllAccompanyPlayCategory(){
-        return findByPid(CategoryParentEnum.ACCOMPANY_PLAY.getType(),true);
+    public List<Category> findAllAccompanyPlayCategory() {
+        return findByPid(CategoryParentEnum.ACCOMPANY_PLAY.getType(), true);
     }
 
     @Override
-    public List<Category> findByPid(Integer pid,Boolean status) {
+    public List<Category> findByPid(Integer pid, Boolean status) {
         PageHelper.orderBy("sort desc");
         CategoryVO categoryVO = new CategoryVO();
         categoryVO.setPid(pid);
         categoryVO.setStatus(status);
-        List<Category> categoryList =categoryDao.findByParameter(categoryVO);
+        List<Category> categoryList = categoryDao.findByParameter(categoryVO);
         return categoryList;
     }
 
