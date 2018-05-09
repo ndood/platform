73910bb2663f107f0d8a4d27c1e1246329b774f6
bwang <burgleaf@163.com>
2018-05-09 20:38:09 +0800

    修改接单短信提醒模板

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/SMSTemplateEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/SMSTemplateEnum.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum SMSTemplateEnum implements TypeEnum<String>{
 
     VERIFICATION_CODE("245842","短信验证码"),
-    ORDER_RECEIVING_REMIND("245844","接单提醒");
+    ORDER_RECEIVING_REMIND("247895","接单提醒");
 
     private String type;
     private String msg;
