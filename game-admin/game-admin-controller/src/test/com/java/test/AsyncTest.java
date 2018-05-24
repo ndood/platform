7cf8b9efd4a9bf4183fb25c7fead4aed44ba8ed3
bwang <burgleaf@163.com>
2018-05-24 18:33:27 +0800

    添加用户认证接口

--- game-admin/game-admin-controller/src/test/com/java/test/AsyncTest.java
+++ game-admin/game-admin-controller/src/test/com/java/test/AsyncTest.java
@@ -35,4 +35,5 @@ public class AsyncTest {
         Long  result = DateUtil.between(new Date(),date, DateUnit.DAY);
         System.out.println(result);
     }
+
 }
--- game-play/game-play-controller/src/main/java/com/fulu/game/play/controller/AuthController.java
+++ game-play/game-play-controller/src/main/java/com/fulu/game/play/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.fulu.game.core.entity.vo.UserInfoAuthVO;
 import com.fulu.game.core.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,31 @@ public class AuthController extends BaseController{
     @Autowired
     private UserInfoAuthService userInfoAuthService;
 
-
+    /**
+     * 保存认证信息
+     * @param userInfoAuthVO
+     * @return
+     */
+    @PostMapping(value = "/info/save")
     public Result userAuthSave(UserInfoAuthVO userInfoAuthVO){
         userInfoAuthService.save(userInfoAuthVO);
         return Result.success().data(userInfoAuthVO);
     }
+
+    /**
+     * 认证信息查询
+     * @param userId
+     * @return
+     */
+    @PostMapping(value = "/info/query")
+    public Result userAuthQuery(Integer userId){
+        UserInfoAuthVO userInfoAuthVO =userInfoAuthService.findUserAuthInfoByUserId(userId);
+        return Result.success().data(userInfoAuthVO);
+    }
+
+
+
+
 
 
 
