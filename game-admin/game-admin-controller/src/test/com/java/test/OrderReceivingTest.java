c7727b8c37b575ac191d3fa40e06c34763181c9d
bwang <burgleaf@163.com>
2018-05-25 18:43:59 +0800

    修改技能状态

--- game-admin/game-admin-controller/src/test/com/java/test/OrderReceivingTest.java
+++ game-admin/game-admin-controller/src/test/com/java/test/OrderReceivingTest.java
@@ -2,6 +2,8 @@ package com.java.test;
 
 import com.google.common.collect.Lists;
 import com.xiaoleilu.hutool.io.FileUtil;
+import lombok.Data;
+import lombok.Getter;
 import org.junit.Test;
 
 import java.util.*;
@@ -13,7 +15,7 @@ public class OrderReceivingTest {
 
     @Test
     public void test() {
-        List<String> lines = FileUtil.readLines("E:\\data\\0523.log", "utf-8");
+        List<String> lines = FileUtil.readLines("E:\\data\\524.log", "utf-8");
         // 创建 Pattern 对象
         Pattern genderPattern = Pattern.compile("gender=2");
 
@@ -24,67 +26,74 @@ public class OrderReceivingTest {
             }
         }
 
-        //查找所有接单用户的ID
-        Pattern startOrderPattern = Pattern.compile("开始接单设置:userId:(\\d+);");
-        Set<Integer> userIds = new LinkedHashSet<>();
+        //查找所有开始接单的记录
+        List<String> errLog = new ArrayList<>();
+        Pattern startOrderPattern = Pattern.compile("开始接单设置:userId:(\\d+).+categoryId=(\\d+),");
+        Pattern beginPattern = Pattern.compile("开始接单设置:userId:(\\d+);\\(id=(\\d+),.+categoryId=(\\d+),.+;beginOrderDate:(\\d+);orderHour:(\\d\\.\\d);");
+        Map<String,List<OrderStatistics>> startMap = new LinkedHashMap<>();
         for (String line : womanLines) {
             Matcher matcher = startOrderPattern.matcher(line);
             if(matcher.find()){
-                userIds.add(Integer.valueOf(matcher.group(1)));
-            }
-        }
-
-        //按用户分类所有的日志
-        Map<Integer,List<String>> userMap = new LinkedHashMap<>();
-        for(Integer userId: userIds){
-            Pattern userOrderPattern = Pattern.compile("接单设置:userId:"+userId+";");
-            for (String line : womanLines) {
-                Matcher matcher = userOrderPattern.matcher(line);
-                if(matcher.find()){
-                    if(userMap.containsKey(userId)){
-                        userMap.get(userId).add(line);
+                Matcher beginMatcher = beginPattern.matcher(line);
+                if(beginMatcher.find()){
+                    String userCategoryKey = beginMatcher.group(1)+"-"+beginMatcher.group(2);
+                    OrderStatistics orderStatistics = new OrderStatistics();
+                    orderStatistics.setUserId(beginMatcher.group(1));
+                    orderStatistics.setCategoryId(beginMatcher.group(2));
+                    orderStatistics.setBeginOrderDate(beginMatcher.group(3));
+                    orderStatistics.setOrderHour(beginMatcher.group(4));
+                    if(startMap.containsKey(userCategoryKey)){
+                        startMap.get(userCategoryKey).add(orderStatistics);
                     }else{
-                        userMap.put(userId, Lists.newArrayList(line));
+                        startMap.put(userCategoryKey,Lists.newArrayList(orderStatistics));
                     }
+                }else{
+                    errLog.add(line);
+                }
+            }
+        }
+        //查找所有结束接单的记录
+        Pattern endOrderPattern = Pattern.compile("停止接单设置:userId:(\\d+).+categoryId=(\\d+),");
+        Pattern endPattern = Pattern.compile("停止接单设置:userId:(\\d+);\\(id=(\\d+),.+categoryId=(\\d+),.+;endOrderDate:(\\d+);");
+        Map<String,List<OrderStatistics>> endMap = new LinkedHashMap<>();
+        for (String line : womanLines) {
+            Matcher matcher = endOrderPattern.matcher(line);
+            if(matcher.find()){
+                Matcher endMatcher = endPattern.matcher(line);
+                if(endMatcher.find()){
+                    String userCategoryKey = endMatcher.group(1)+"-"+endMatcher.group(2);
+                    OrderStatistics orderStatistics = new OrderStatistics();
+                    orderStatistics.setUserId(endMatcher.group(1));
+                    orderStatistics.setCategoryId(endMatcher.group(2));
+                    orderStatistics.setEndOrderDate(endMatcher.group(3));
+                    if(endMap.containsKey(userCategoryKey)){
+                        endMap.get(userCategoryKey).add(orderStatistics);
+                    }else{
+                        endMap.put(userCategoryKey,Lists.newArrayList(orderStatistics));
+                    }
+                }else{
+                    errLog.add(line);
                 }
             }
         }
 
-        //查询满足两个用户商品的用户
-        Set<Integer> rightUser = new LinkedHashSet<>();
-        userMap.forEach((k,v)->{
-            v.forEach((line)->{
-                int size = v.size();
-                if(size>=4){
-                    rightUser.add(k);
-                }
-            });
-        });
 
-        //排除停止接单的用户
-        userMap.forEach((k,v)->{
-            if(rightUser.contains(k)){
-                Pattern userOrderPattern = Pattern.compile("停止接单设置");
-                final int[] size = {v.size()};
-                v.forEach((line)->{
-                    if(userOrderPattern.matcher(line).find()){
-                        size[0]--;
-                    }
-                });
-                if(size[0]<4){
-                    rightUser.remove(k);
-                }
-            }
-        });
-
-
-        userMap.forEach((k,v)->{
-            if(rightUser.contains(k)){
-                FileUtil.writeLines(v,"E:\\data\\0523_result.log","utf-8",true);
-            }
-        });
-
+        System.out.println(startMap);
+        System.out.println(endMap);
+        FileUtil.writeLines(errLog,"E:\\data\\524-err.log","utf-8",false);
 
     }
+
+    @Data
+    class OrderStatistics{
+        private String userId;
+        private String categoryId;
+        private String productId;
+        private String beginOrderDate;
+        private String orderHour;
+        private String endOrderDate;
+    }
+
+
 
 }
--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/TechAuthStatusEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/TechAuthStatusEnum.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum TechAuthStatusEnum implements TypeEnum<Integer>{
-
+    NO_AUTHENTICATION(0,"未认证"),
     AUTHENTICATION_ING(1,"认证中"),
     NORMAL(2,"正常"),
     FREEZE(3,"冻结");
