970848b33e353657a8449681e3b29a3a68e160fc
bwang <burgleaf@163.com>
2018-06-07 20:21:51 +0800

    还原ACL代码

--- game-play/game-play-controller/src/main/java/com/fulu/game/play/shiro/AclFilter.java
+++ game-play/game-play-controller/src/main/java/com/fulu/game/play/shiro/AclFilter.java
@@ -48,10 +48,9 @@ public class AclFilter extends AccessControlFilter {
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String token = httpRequest.getHeader("token");
-        log.info("请求header中的token {}", token);
-        Map<String, Object> map = redisOpenService.hget(RedisKeyEnum.PLAY_TOKEN.generateKey(token));
+        log.info("head中的token:{}", token);
         // 没有登录授权 且没有记住我
-        if (MapUtils.isEmpty(map)) {
+        if (!redisOpenService.hasKey(RedisKeyEnum.PLAY_TOKEN.generateKey(token))) {
             log.info("token {} 验证失效=====", token);
             // 没有登录
             HttpServletResponse httpResponse = (HttpServletResponse) response;
@@ -65,9 +64,8 @@ public class AclFilter extends AccessControlFilter {
                 res.put("data", "");
                 res.put("msg", "您未登录，暂无访问权限！");
                 out.write(res.toString());
-                log.info("acl返回501,map:{};", map);
             } catch (IOException e) {
-                log.error("IO错误:", e);
+                log.error("IO异常:{}", e);
             } finally {
                 if (out != null) {
                     out.close();
@@ -75,8 +73,12 @@ public class AclFilter extends AccessControlFilter {
             }
             return false;
         }
-        //在存5分钟，保证会话时长
+        Map<String, Object> map = redisOpenService.hget(RedisKeyEnum.PLAY_TOKEN.generateKey(token));
         redisOpenService.hset(RedisKeyEnum.PLAY_TOKEN.generateKey(token), map);
+        //sessionKey 时间用户token时间保持一致
+        String sessionKey = redisOpenService.get(RedisKeyEnum.WX_SESSION_KEY.generateKey(token));
+        redisOpenService.set(RedisKeyEnum.WX_SESSION_KEY.generateKey(token),sessionKey);
+
         //已登录的，就保存该token从redis查到的用户信息
         User user = BeanUtil.mapToBean(map, User.class, true);
         SubjectUtil.setCurrentUser(user);
