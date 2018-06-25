796c17c94ce5e6bdd8110bdd87f5ea7d4ffc67d5
bwang <burgleaf@163.com>
2018-06-25 19:24:20 +0800

    添加价格系数接口

--- game-play/game-play-controller/src/main/java/com/fulu/game/play/controller/PriceFactorController.java
+++ game-play/game-play-controller/src/main/java/com/fulu/game/play/controller/PriceFactorController.java
@@ -1,16 +1,35 @@
 package com.fulu.game.play.controller;
 
 
+import com.fulu.game.common.Result;
+import com.fulu.game.core.entity.PriceFactor;
+import com.fulu.game.core.service.PriceFactorService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(value = "/factor/v1/")
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping(value = "/api/v1/factor")
 @Slf4j
 public class PriceFactorController extends BaseController{
 
+    @Autowired
+    private PriceFactorService priceFactorService;
 
+
+    @PostMapping(value = "/get")
+    public Result priceFactor(){
+        PriceFactor priceFactor = priceFactorService.findByNewPriceFactor();
+        if(priceFactor==null){
+            return Result.success().data(1);
+        }
+        return Result.success().data(priceFactor.getFactor());
+    }
 
 
 }
