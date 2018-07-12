8cc0cca8001e429f6085ba708c8957fcf59c481a
Gong Zechun <gongzechun@fulu.com>
2018-07-12 11:47:04 +0800

    [feature]新增接口：管理员-拒绝打款（用户的提现申请被管理员拒绝）

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/MoneyOperateTypeEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/MoneyOperateTypeEnum.java
@@ -10,6 +10,7 @@ public enum MoneyOperateTypeEnum implements TypeEnum<Integer>{
     USER_DRAW_CASH(-1, "用户提款"),
     ADMIN_ADD_CHANGE(1, "管理员加零钱"),
     ORDER_COMPLETE(2, "陪玩订单完成"),
+    ADMIN_REFUSE_REMIT(3, "管理员拒绝打款"),
 
     CHANNEL_ADD_CASH(1, "渠道商加款"),
     CHANNEL_CUT_CASH(2, "渠道商扣款"),
