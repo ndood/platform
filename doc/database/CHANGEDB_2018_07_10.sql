d9fe77fc79136e0dbce0d94624c60fa70cb88093
bwang <burgleaf@163.com>
2018-07-12 20:50:18 +0800

    修改SQL

--- doc/database/CHANGEDB_2018_07_10.sql
+++ doc/database/CHANGEDB_2018_07_10.sql
@@ -16,4 +16,3 @@ COMMENT '-1提款，1加零钱，2陪玩订单入账，3拒绝提现返款';
 -- t_cash_draws表修改cash_status字段注释
 ALTER TABLE `t_cash_draws` MODIFY COLUMN `cash_status` int(3) DEFAULT '0'
 COMMENT '申请单处理状态（0：未处理，1：已打款，2：已拒绝）';
-
