62c68b4144655fb5e0cca0c40691fd2573155d06
Gong Zechun <gongzechun@fulu.com>
2018-07-31 20:58:02 +0800

    [feature]管理后台 未接单订单列表 增加两个查询条件：userMobile和serviceUserMobile

--- game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/OrderServiceImpl.java
+++ game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/OrderServiceImpl.java
@@ -136,7 +136,7 @@ public class OrderServiceImpl extends AbsCommonService<Order, Integer> implement
 
     public void pushToServiceOrderWxMessage(Order order, WechatTemplateMsgEnum wechatTemplateMsgEnum) {
         if (OrderTypeEnum.POINT.getType().equals(order.getType())) {
-            if(order.getServiceUserId()!=null){
+            if (order.getServiceUserId() != null) {
                 wxTemplateMsgService.pushWechatTemplateMsg(order.getServiceUserId(), wechatTemplateMsgEnum.choice(WechatEcoEnum.POINT.getType()));
             }
         } else {
@@ -147,7 +147,7 @@ public class OrderServiceImpl extends AbsCommonService<Order, Integer> implement
     public void pushToUserOrderWxMessage(Order order, WechatTemplateMsgEnum wechatTemplateMsgEnum) {
         String orderStatus = OrderStatusEnum.getMsgByStatus(order.getStatus());
         if (OrderTypeEnum.POINT.getType().equals(order.getType())) {
-            if(order.getServiceUserId()!=null){
+            if (order.getServiceUserId() != null) {
                 wxTemplateMsgService.pushWechatTemplateMsg(order.getUserId(), wechatTemplateMsgEnum.choice(WechatEcoEnum.POINT.getType()), orderStatus);
             }
         } else {
@@ -282,6 +282,7 @@ public class OrderServiceImpl extends AbsCommonService<Order, Integer> implement
 
     /**
      * 上分订单抢单
+     *
      * @param orderNo
      * @return
      */
@@ -1589,9 +1590,7 @@ public class OrderServiceImpl extends AbsCommonService<Order, Integer> implement
             orderBy = "create_time desc";
         }
         PageHelper.startPage(pageNum, pageSize, orderBy);
-        orderSearchVO.setType(OrderTypeEnum.POINT.getType());
-        orderSearchVO.setStatus(OrderStatusEnum.WAIT_SERVICE.getStatus());
-        List<Order> orderList = findBySearchVO(orderSearchVO);
+        List<Order> orderList = orderDao.unacceptOrderList(orderSearchVO);
         if (CollectionUtil.isEmpty(orderList)) {
             return null;
         }
