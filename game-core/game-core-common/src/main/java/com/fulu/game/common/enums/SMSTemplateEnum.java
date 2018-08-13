9c4bc30207f9efae1c4d8ea938ffe5b9ea120d9a
bwang <burgleaf@163.com>
2018-08-13 18:38:50 +0800

    修改短信模板

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/SMSTemplateEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/SMSTemplateEnum.java
@@ -9,7 +9,7 @@ public enum SMSTemplateEnum implements TypeEnum<String>{
 
     VERIFICATION_CODE("245842","短信验证码"),
     ORDER_RECEIVING_REMIND("247920","接单提醒"),
-    SENDLEAVE_INFORM("260498","开黑留言");
+    SENDLEAVE_INFORM("314670","开黑留言");
 
     private String type;
     private String msg;
