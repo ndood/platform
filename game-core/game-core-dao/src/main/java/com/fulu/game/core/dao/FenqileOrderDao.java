ccf030736ffb813151daa830b2f0cd38dfa04491
Gong Zechun <gongzechun@fulu.com>
2018-08-15 15:04:03 +0800

    [feature]分期乐订单接口

--- game-core/game-core-dao/src/main/java/com/fulu/game/core/dao/FenqileOrderDao.java
+++ game-core/game-core-dao/src/main/java/com/fulu/game/core/dao/FenqileOrderDao.java
@@ -2,20 +2,24 @@ package com.fulu.game.core.dao;
 
 import com.fulu.game.core.entity.FenqileOrder;
 import com.fulu.game.core.entity.vo.FenqileOrderVO;
+import com.fulu.game.core.entity.vo.searchVO.FenqileOrderSearchVO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-
 /**
  * 分期乐订单拓展表
+ *
  * @author Gong Zechun
  * @email ${email}
  * @date 2018-08-14 18:19:17
  */
 @Mapper
-public interface FenqileOrderDao extends ICommonDao<FenqileOrder,Integer>{
+public interface FenqileOrderDao extends ICommonDao<FenqileOrder, Integer> {
 
     List<FenqileOrder> findByParameter(FenqileOrderVO fenqileOrderVO);
 
+    List<FenqileOrderVO> list(FenqileOrderSearchVO searchVO);
+
+    FenqileOrderVO getTotalReconAmount(FenqileOrderSearchVO searchVO);
 }
---
+++ game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/vo/searchVO/FenqileOrderSearchVO.java
@@ -0,0 +1,11 @@
+package com.fulu.game.core.entity.vo.searchVO;
+
+/**
+ * todo：描述文字
+ *
+ * @author Gong ZeChun
+ * @date 2018/8/15 10:21
+ */
+public class FenqileOrderSearchVO extends OrderSearchVO {
+
+}
