39487fdb84be1df44807f656bd370598f6fca2be
ancyshi <ancyshi@163.com>
2018-08-15 09:36:39 +0800

    分期乐添加-提交建议接口

--- game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/fenqile/UserController.java
+++ game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/fenqile/UserController.java
@@ -1,12 +1,16 @@
 package com.fulu.game.h5.controller.fenqile;
 
 import com.fulu.game.common.Result;
+import com.fulu.game.core.entity.Advice;
 import com.fulu.game.core.entity.vo.UserCommentVO;
+import com.fulu.game.core.service.AdviceService;
 import com.fulu.game.core.service.UserCommentService;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,9 +25,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController extends BaseController {
     private final UserCommentService commentService;
 
+    private AdviceService adviceService;
+
     @Autowired
-    public UserController(UserCommentService commentService) {
+    public UserController(UserCommentService commentService, AdviceService adviceService) {
         this.commentService = commentService;
+        this.adviceService = adviceService;
     }
 
     /**
@@ -40,5 +47,24 @@ public class UserController extends BaseController {
                                       Integer serverId) {
         PageInfo<UserCommentVO> page = commentService.findByServerId(pageNum, pageSize, serverId);
         return Result.success().data(page);
+    }
+
+
+    /**
+     * 提交建议
+     * @param content 建议内容
+     * @param contact 联系方式【电话或者QQ】
+     * @param advicePicUrls 建议截图
+     * @return
+     */
+    @PostMapping("/advice/add")
+    public Result addAdvice(@RequestParam("content") String content,
+                            @RequestParam(value = "contact", required = false) String contact,
+                            @RequestParam(value = "advicePicUrls", required = false) String[] advicePicUrls) {
+        if (content == null) {
+            return Result.error().msg("请填写建议内容");
+        }
+        Advice advice = adviceService.addAdvice(content, contact, advicePicUrls);
+        return Result.success().data(advice).msg("提交成功");
     }
 }
