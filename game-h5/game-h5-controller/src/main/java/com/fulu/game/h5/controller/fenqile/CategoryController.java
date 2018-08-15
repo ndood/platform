46c8c47b0c520be037322e75110e87ab06657a8f
denggr <20160227fulu>
2018-08-15 09:35:57 +0800

    增加分期乐首页接口

--- game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/fenqile/CategoryController.java
+++ game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/fenqile/CategoryController.java
@@ -22,33 +22,21 @@ import java.util.List;
 /**
  * 分类Controller
  *
- * @author Gong ZeChun
- * @date 2018/8/13 15:53
+ * @author jaycee.deng
+ * @date 2018/8/14 15:53
  */
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/category")
 public class CategoryController extends BaseController {
-    private final TechAttrService techAttrService;
     private final CategoryService categoryService;
     private final ProductService productService;
-    private final SalesModeService salesModeService;
-    private final TagService tagService;
-    private final TechValueService techValueService;
 
     @Autowired
-    public CategoryController(TechAttrService techAttrService,
-                              CategoryService categoryService,
-                              ProductService productService,
-                              SalesModeService salesModeService,
-                              TagService tagService,
-                              TechValueService techValueService) {
-        this.techAttrService = techAttrService;
+    public CategoryController(CategoryService categoryService,
+                              ProductService productService) {
         this.categoryService = categoryService;
         this.productService = productService;
-        this.salesModeService = salesModeService;
-        this.tagService = tagService;
-        this.techValueService = techValueService;
     }
 
     /**
@@ -66,20 +54,7 @@ public class CategoryController extends BaseController {
         }
         return Result.success().data(categoryList);
     }
-
-    /**
-     * 分页查询分期乐业务
-     *
-     * @param pageNum  页码
-     * @param pageSize 每页显示数据条数
-     * @return 封装结果集
-     */
-    @PostMapping(value = "list")
-    public Result page(@RequestParam Integer pageNum,
-                       @RequestParam Integer pageSize) {
-        PageInfo<Category> categoryList = categoryService.list(pageNum, pageSize);
-        return Result.success().data(categoryList);
-    }
+    
 
     /**
      * 分页查询所有商品
@@ -102,47 +77,4 @@ public class CategoryController extends BaseController {
         return Result.success().data(pageInfo);
     }
 
-    /**
-     * 根据业务查询游戏销售方式
-     *
-     * @param categoryId 分类id
-     * @return 封装结果集
-     */
-    @PostMapping(value = "/salesmode/list")
-    public Result saleModel(@RequestParam Integer categoryId) {
-        List<SalesMode> salesModeList = salesModeService.findByCategory(categoryId);
-        return Result.success().data(salesModeList);
-    }
-
-
-    /**
-     * 查询游戏所有标签
-     *
-     * @param categoryId 分类id
-     * @return 封装结果集
-     */
-    @PostMapping(value = "/tag/list")
-    public Result techTags(@RequestParam Integer categoryId) {
-        TagVO tagVO = tagService.oldFindTagsByCategoryId(categoryId);
-        if (tagVO == null) {
-            return Result.error().msg("该游戏没有设置标签!");
-        }
-        return Result.success().data(tagVO);
-    }
-
-    /**
-     * 查询游戏所有段位
-     *
-     * @param categoryId 分类id
-     * @return 封装结果集
-     */
-    @PostMapping(value = "/dan/list")
-    public Result danList(@RequestParam Integer categoryId) {
-        TechAttr techAttr = techAttrService.findByCategoryAndType(categoryId, TechAttrTypeEnum.DAN.getType());
-        if (techAttr == null) {
-            return Result.error().msg("该游戏没有设置段位信息!");
-        }
-        List<TechValue> techValueList = techValueService.findByTechAttrId(techAttr.getId());
-        return Result.success().data(techValueList);
-    }
 }
