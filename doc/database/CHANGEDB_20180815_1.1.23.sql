335996a06617a2ca04fd07266cd017d578d5f78e
Gong Zechun <gongzechun@fulu.com>
2018-08-16 10:08:32 +0800

    [feature]sql提交

--- doc/database/CHANGEDB_20180815_1.1.23.sql
+++ doc/database/CHANGEDB_20180815_1.1.23.sql
@@ -26,3 +26,20 @@ CREATE TABLE `t_fenqile_reconciliation` (
   `create_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) COMMENT='分期乐对账表';
+
+--分期乐对账记录表
+DROP TABLE IF EXISTS `t_fenqile_recon_record`;
+CREATE TABLE `t_fenqile_recon_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `start_time` datetime DEFAULT NULL COMMENT '订单开始时间',
+  `end_time` datetime DEFAULT NULL COMMENT '订单结束时间',
+  `amount` decimal(11,2) DEFAULT '0.00' COMMENT '对账金额',
+  `order_count` int(11) DEFAULT NULL COMMENT '订单数量',
+  `process_time` datetime DEFAULT NULL COMMENT '对账时间',
+  `admin_id` int(11) DEFAULT NULL COMMENT '对账人id',
+  `admin_name` varchar(255) DEFAULT NULL COMMENT '对账人用户名',
+  `remark` varchar(1000) DEFAULT NULL COMMENT '备注',
+  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) COMMENT='分期乐对账记录表';
