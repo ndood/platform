7f9cde9813b6d7225bea3885a2075d0d6b452eab
Gong Zechun <gongzechun@fulu.com>
2018-08-23 18:59:35 +0800

    [feature]微信模板推送修改

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/WechatTemplateIdEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/WechatTemplateIdEnum.java
@@ -11,7 +11,7 @@ public enum WechatTemplateIdEnum {
     PLAY_LEAVE_MSG("yD7JulFzNv7ZNInswmn6_hdgwlf68qRL0fwLUNq98Vc", "留言消息模板"),
     POINT_LEAVE_MSG("q4vzN44BKXw-ZDLH-g7qsPgFb6UEj0enkutCwNfAuh0", "留言消息模板"),
     PLAY_SERVICE_PROCESS_NOTICE("tXFoUhfqrMRADhrF4Sp1yaiUwpw6H6XwBc_xf1LJzo8", "服务进度通知模板"),
-    POINT_SERVICE_PROCESS_NOTICE("", "服务进度通知模板");
+    POINT_SERVICE_PROCESS_NOTICE("pO3Wjr5bbtiublql1uV4gfdEypWO7g3z5_zFdLa8-Wg", "服务进度通知模板");
 
     private String templateId;
     private String msg;
