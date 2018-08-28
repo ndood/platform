f121cc35264806eed443aee7c151eda92ae89be5
bwang <burgleaf@163.com>
2018-08-28 15:49:02 +0800

    添加用户IM接口

--- game-app/game-app-controller/src/main/java/com/fulu/game/app/controller/UserController.java
+++ game-app/game-app-controller/src/main/java/com/fulu/game/app/controller/UserController.java
@@ -4,15 +4,19 @@ import cn.hutool.core.date.DateUtil;
 import com.fulu.game.common.Result;
 import com.fulu.game.common.domain.ClientInfo;
 import com.fulu.game.common.enums.RedisKeyEnum;
+import com.fulu.game.common.exception.UserException;
 import com.fulu.game.common.utils.OssUtil;
 import com.fulu.game.common.utils.SubjectUtil;
 import com.fulu.game.core.entity.AdminImLog;
+import com.fulu.game.core.entity.ImUser;
 import com.fulu.game.core.entity.User;
 import com.fulu.game.core.entity.vo.UserVO;
 import com.fulu.game.core.service.AdminImLogService;
+import com.fulu.game.core.service.ImService;
 import com.fulu.game.core.service.UserService;
 import com.fulu.game.core.service.impl.RedisOpenServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,8 +39,8 @@ public class UserController extends BaseController {
     private RedisOpenServiceImpl redisOpenService;
     @Autowired
     private AdminImLogService adminImLogService;
-
-
+    @Autowired
+    private ImService imService;
     /**
      * 修改/填写资料
      * @param userVO
@@ -85,6 +89,62 @@ public class UserController extends BaseController {
             redisOpenService.delete(RedisKeyEnum.USER_ONLINE_KEY.generateKey(user.getId()));
         }
         return Result.success();
+    }
+
+
+    @PostMapping("/im/save")
+    public Result imSave(@RequestParam("status") Integer status,
+                         @RequestParam("imId") String imId,
+                         @RequestParam("imPsw") String imPsw,
+                         @RequestParam(value = "errorMsg", required = false) String errorMsg) {
+        User user = userService.findById(userService.getCurrentUser().getId());
+        if (null == user) {
+            log.info("当前用户id={}查询数据库不存在，无法绑定", userService.getCurrentUser().getId());
+            throw new UserException(UserException.ExceptionCode.USER_NOT_EXIST_EXCEPTION);
+        }
+        log.info("IM注册请求开始,请求参数status:{},user:{},imId={},imPsw={},errorMsg={}", user, status, imId, imPsw, errorMsg);
+        if (user.getImId() != null) {
+            log.info("用户IM信息已经存在:user:{};", user);
+            return Result.success().data(user).msg("已存在IM账号");
+        }
+        if (status.equals(200)) {
+            user.setImId(imId);
+            user.setImPsw(imPsw);
+            user.setUpdateTime(new Date());
+            userService.update(user);
+            userService.updateRedisUser(user);
+            log.info("用户:{}绑定IM信息成功:user:{};", user.getId(), user);
+        } else if (status.equals(500)) {
+            String newIMId = "s" + imId;
+            ImUser imUser = imService.registerUser(newIMId, imPsw);
+            user.setImId(imUser.getUsername());
+            user.setImPsw(imUser.getImPsw());
+            user.setUpdateTime(new Date());
+            userService.update(user);
+            userService.updateRedisUser(user);
+            log.error("用户:{}绑定IM失败,失败原因:{}", user, errorMsg);
+        }
+        return Result.success().data(user).msg("IM用户信息保存成功！");
+    }
+
+    /**
+     * 用户-根据imId查询聊天对象User
+     *
+     * @return
+     */
+    @PostMapping("/im/get")
+    public Result getImUser(@RequestParam("imId") String imId,
+                            String content) {
+        log.info("根据imId获取用户信息:imId:{};content:{}", imId, content);
+        if (StringUtils.isEmpty(imId)) {
+            throw new UserException(UserException.ExceptionCode.IllEGAL_IMID_EXCEPTION);
+        }
+        User user = userService.findByImId(imId);
+        if (null == user) {
+            return Result.error().msg("未查询到该用户或尚未注册IM");
+        }
+        log.info("根据imId获取用户信息:imId:{};content:{};user:{}", imId, content, user);
+        return Result.success().data(user).msg("查询IM用户成功");
     }
 
 }
