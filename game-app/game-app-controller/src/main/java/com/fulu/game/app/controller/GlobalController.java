847f14d34b271338377a05f59815a91956d12305
bwang <burgleaf@163.com>
2018-08-30 10:02:42 +0800

    添加app service

--- game-app/game-app-controller/src/main/java/com/fulu/game/app/controller/GlobalController.java
+++ game-app/game-app-controller/src/main/java/com/fulu/game/app/controller/GlobalController.java
@@ -1,18 +1,14 @@
 package com.fulu.game.app.controller;
 
 
-import com.fulu.game.common.Constant;
 import com.fulu.game.common.Result;
-import com.fulu.game.common.enums.PicExampleEnum;
-import com.fulu.game.common.enums.RedisKeyEnum;
-import com.fulu.game.common.utils.GenIdUtil;
 import com.fulu.game.common.utils.OssUtil;
-import com.fulu.game.core.entity.User;
-import com.fulu.game.core.service.UserService;
-import com.fulu.game.core.service.impl.RedisOpenServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -24,15 +20,11 @@ public class GlobalController extends BaseController {
     private OssUtil ossUtil;
 
 
-
     @PostMapping(value = "upload")
     public Result upload(@RequestParam("file") MultipartFile file, String name) throws Exception {
         String fileName = ossUtil.uploadFile(file.getInputStream(), file.getOriginalFilename());
         return Result.success().data(fileName).msg(name);
     }
-
-
-
 
 
 }
---
+++ game-app/game-app-service-impl/src/main/java/com/fulu/game/app/service/impl/AppOrderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.fulu.game.app.service.impl;
+
+
+import com.fulu.game.core.service.impl.AbOrderOpenServiceImpl;
+import com.fulu.game.core.service.impl.push.MiniAppPushServiceImpl;
+
+import java.math.BigDecimal;
+
+public class AppOrderServiceImpl {
+
+
+}
