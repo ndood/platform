e3a3a37f3f39ba4ea40b3450e8c6fbc1a062230d
Gong Zechun <gongzechun@fulu.com>
2018-08-30 15:16:15 +0800

    [refactor]修改表结构

--- game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/VirtualProduct.java
+++ game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/VirtualProduct.java
@@ -10,7 +10,7 @@ import java.util.Date;
  * 虚拟商品表
  *
  * @author Gong Zechun
- * @date 2018-08-30 15:02:01
+ * @date 2018-08-30 15:14:16
  */
 @Data
 public class VirtualProduct implements Serializable {
@@ -24,10 +24,14 @@ public class VirtualProduct implements Serializable {
     private Integer price;
     //1 礼物  2 私照图片组 3 IM解锁图片组 4 IM解锁语音
     private Integer type;
+    //排序
+    private Integer sort;
     //商品图片地址
     private String objectUrl;
     //
     private String remark;
+    //数据状态  1  正常 0已删除
+    private Integer delFlag;
     //
     private Date updateTime;
     //
