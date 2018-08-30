53a5b2e8fe9cb8c26610f3b1de67987d88bcd260
Gong Zechun <gongzechun@fulu.com>
2018-08-30 15:37:13 +0800

    [feature]虚拟商品种类枚举类

---
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/VirtualProductTypeEnum.java
@@ -0,0 +1,22 @@
+package com.fulu.game.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 虚拟商品种类枚举类
+ *
+ * @author Gong ZeChun
+ * @date 2018/8/30 15:09
+ */
+@Getter
+@AllArgsConstructor
+public enum VirtualProductTypeEnum implements TypeEnum<Integer> {
+    VIRTUAL_GIFT(1, "礼物"),
+    PERSONAL_PICS(2, "私照图片组"),
+    IM_PROTECTED_PICS(3, "IM解锁图片组"),
+    IM_PROTECTED_VOICE(4, "IM解锁语音");
+
+    private Integer type;
+    private String msg;
+}
