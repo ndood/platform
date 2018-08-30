ac3740db3ffe8df8ab097bf19bb92f3f33594dd2
Gong Zechun <gongzechun@fulu.com>
2018-08-30 10:08:35 +0800

    [feature]提交

--- game-app/game-app-service-impl/src/main/java/com/fulu/game/app/service/impl/AppOrderServiceImpl.java
+++ game-app/game-app-service-impl/src/main/java/com/fulu/game/app/service/impl/AppOrderServiceImpl.java
@@ -1,12 +1,32 @@
 package com.fulu.game.app.service.impl;
 
 
+import com.fulu.game.core.entity.Order;
 import com.fulu.game.core.service.impl.AbOrderOpenServiceImpl;
 import com.fulu.game.core.service.impl.push.MiniAppPushServiceImpl;
 
 import java.math.BigDecimal;
 
-public class AppOrderServiceImpl {
+public class AppOrderServiceImpl extends AbOrderOpenServiceImpl{
 
 
+    @Override
+    protected void dealOrderAfterPay(Order order) {
+
+    }
+
+    @Override
+    protected void shareProfit(Order order) {
+
+    }
+
+    @Override
+    protected void orderRefund(Order order, BigDecimal refundMoney) {
+
+    }
+
+    @Override
+    protected MiniAppPushServiceImpl getMinAppPushService() {
+        return null;
+    }
 }
