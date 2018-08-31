90417d1e685f04cce96f2e7180b53ea84b4a1125
ancyshi <ancyshi@163.com>
2018-08-31 18:35:12 +0800

    我的_用户兴趣修改返回时间格式

--- game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/UserInterests.java
+++ game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/UserInterests.java
@@ -2,6 +2,8 @@ package com.fulu.game.core.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 
@@ -22,8 +24,10 @@ public class UserInterests implements Serializable {
 	//排序号
 	private Integer sort;
 	//创建时间
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
 	private Date createTime;
 	//修改时间
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
 	private Date updateTime;
 	//数据状态(1：有效；0：无效)
 	private Integer status;
