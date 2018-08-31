461ebcdd6a6fbf22d202dca2910df3de46ea96e4
Gong Zechun <gongzechun@fulu.com>
2018-08-31 14:52:43 +0800

    [feature]生成虚拟商品订单号

--- game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/VirtualProductOrderServiceImpl.java
+++ game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/VirtualProductOrderServiceImpl.java
@@ -1,9 +1,12 @@
 package com.fulu.game.core.service.impl;
 
 
+import cn.hutool.core.date.DateUtil;
+import com.fulu.game.common.enums.VirtualDetailsTypeEnum;
 import com.fulu.game.common.enums.VirtualProductTypeEnum;
 import com.fulu.game.common.exception.ServiceErrorException;
 import com.fulu.game.common.exception.UserException;
+import com.fulu.game.common.utils.GenIdUtil;
 import com.fulu.game.core.dao.ICommonDao;
 import com.fulu.game.core.dao.VirtualProductOrderDao;
 import com.fulu.game.core.entity.User;
@@ -26,8 +29,10 @@ public class VirtualProductOrderServiceImpl extends AbsCommonService<VirtualProd
     private VirtualDetailsService virtualDetailsService;
     @Autowired
     private VirtualProductService virtualProductService;
+    //    @Autowired
+//    private UserInfoAuthService userInfoAuthService;
     @Autowired
-    private UserInfoAuthService userInfoAuthService;
+    private OrderService orderService;
 
     @Override
     public ICommonDao<VirtualProductOrder, Integer> getDao() {
@@ -39,6 +44,9 @@ public class VirtualProductOrderServiceImpl extends AbsCommonService<VirtualProd
         userService.isCurrentUser(fromUserId);
         User fromUser = userService.findById(fromUserId);
         Integer virtualBalance = fromUser.getVirtualBalance();
+        if (virtualBalance == null) {
+            virtualBalance = 0;
+        }
         Integer price = virtualProductService.findPriceById(virtualProductId);
         if (virtualBalance < price) {
             log.error("用户userId：{}的钻石余额不够送礼物，钻石余额：{}，礼物价值：{}", fromUserId, virtualBalance, price);
@@ -55,14 +63,42 @@ public class VirtualProductOrderServiceImpl extends AbsCommonService<VirtualProd
             throw new UserException(UserException.ExceptionCode.USER_NOT_EXIST_EXCEPTION);
         }
         //发起人扣钻石
-        userService.calculateVirtualBalance(fromUserId, price);
+        userService.calculateVirtualBalance(fromUserId, Math.negateExact(price));
+        //记录订单
+        VirtualProductOrder order = new VirtualProductOrder();
+        order.setOrderNo(generateVirtualProductOrderNo());
+        order.setVirtualProductId(virtualProductId);
+        order.setPrice(price);
+        order.setFromUserId(fromUserId);
+        order.setTargetUserId(targetUserId);
+        order.setRemark(VirtualProductTypeEnum.VIRTUAL_GIFT.getMsg());
+        order.setUpdateTime(DateUtil.date());
+        order.setCreateTime(DateUtil.date());
+        create(order);
+
         //记录流水
-        virtualDetailsService.createVirtualDetails(fromUserId, price, VirtualProductTypeEnum.VIRTUAL_GIFT);
+        virtualDetailsService.createVirtualDetails(fromUserId, virtualProductId, Math.negateExact(price),
+                VirtualDetailsTypeEnum.VIRTUAL_MONEY, VirtualProductTypeEnum.VIRTUAL_GIFT);
         //接收人加魅力值
-        userInfoAuthService.modifyCharm(targetUserId, price);
+//        userInfoAuthService.modifyCharm(targetUserId, price);
         //记录流水
 //        virtualDetailsService.createVirtualDetails(targetUserId, price, VirtualProductTypeEnum.VIRTUAL_GIFT);
 
         return true;
+    }
+
+    /**
+     * 生成虚拟商品订单号
+     *
+     * @return 订单号字符串
+     */
+    @Override
+    public String generateVirtualProductOrderNo() {
+        String orderNo = "V" + GenIdUtil.GetOrderNo();
+        if (orderService.findByOrderNo(orderNo) == null) {
+            return orderNo;
+        } else {
+            return generateVirtualProductOrderNo();
+        }
     }
 }
