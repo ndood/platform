4e7fef7e5252f1ca20d5d9bc4a3fa06252a60d93
Gong Zechun <gongzechun@fulu.com>
2018-08-31 14:22:55 +0800

    [feature]文案修改

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/PlatformEcoEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/PlatformEcoEnum.java
@@ -11,8 +11,8 @@ public enum PlatformEcoEnum implements TypeEnum<Integer> {
     PLAY(1, "开黑陪玩"),
     POINT(2, "开黑上分"),
     H5(3, "H5"),
-    IOS(4, "ANDROID"),
-    ANDROID(5, "IOS"),
+    IOS(4, "IOS"),
+    ANDROID(5, "ANDROID"),
     APP(45, "APP(android+ios)");
 
 
