3ef38417a3d66e8d891a18bd793fbf49efe3f1d5
Gong Zechun <gongzechun@fulu.com>
2018-08-31 15:28:02 +0800

    [feature]官方公告

--- game-core/game-core-dao/src/main/java/com/fulu/game/core/dao/PushMsgDao.java
+++ game-core/game-core-dao/src/main/java/com/fulu/game/core/dao/PushMsgDao.java
@@ -2,28 +2,28 @@ package com.fulu.game.core.dao;
 
 import com.fulu.game.core.entity.PushMsg;
 import com.fulu.game.core.entity.vo.PushMsgVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
 /**
- * 
  * @author wangbin
  * @email ${email}
  * @date 2018-06-06 10:29:12
  */
 @Mapper
-public interface PushMsgDao extends ICommonDao<PushMsg,Integer>{
+public interface PushMsgDao extends ICommonDao<PushMsg, Integer> {
 
     List<PushMsg> findByParameter(PushMsgVO pushMsgVO);
 
     /**
      * 查找当天没有推送的消息
+     *
      * @return
      */
-    List<PushMsg> findNotPushMsgByTouchTime(@Param(value = "beginTime") Date beginTime,@Param(value = "endTime")  Date endTime);
+    List<PushMsg> findNotPushMsgByTouchTime(@Param(value = "beginTime") Date beginTime, @Param(value = "endTime") Date endTime);
 
+    PushMsg newOfficialNotice();
 }
--- game-core/game-core-service/src/main/java/com/fulu/game/core/service/PushMsgService.java
+++ game-core/game-core-service/src/main/java/com/fulu/game/core/service/PushMsgService.java
@@ -33,10 +33,11 @@ public interface PushMsgService extends ICommonService<PushMsg, Integer> {
      *
      * @param pageNum
      * @param pageSize
+     * @param platform
      * @param orderBy
      * @return
      */
-    PageInfo<PushMsg> list(int pageNum, int pageSize, String orderBy);
+    PageInfo<PushMsg> list(Integer pageNum, Integer pageSize, Integer platform, String orderBy);
 
     /**
      * 查找今天没有推送的微信消息
@@ -49,4 +50,11 @@ public interface PushMsgService extends ICommonService<PushMsg, Integer> {
     void hitsStatistics(int pushId);
 
     PageInfo<PushMsg> officialNoticeList(Integer pageNum, Integer pageSize);
+
+    /**
+     * 获取最新的一条官方公告
+     *
+     * @return 官方公告Bean
+     */
+    PushMsg newOfficialNotice();
 }
