9f16de2c54f9df49240e7e80752f56ba115f371f
denggr <20160227fulu>
2018-09-06 15:46:30 +0800

    新增用户身份审核枚举

---
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/UserBodyAuthStatusEnum.java
@@ -0,0 +1,16 @@
+package com.fulu.game.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserBodyAuthStatusEnum implements TypeEnum<Integer>{
+
+    NO_AUTH(0,"未审核"),
+    AUTH_SUCCESS(1,"审核通过"),
+    AUTH_FAIL(2,"审核失败");
+
+    private Integer type;
+    private String msg;
+}
