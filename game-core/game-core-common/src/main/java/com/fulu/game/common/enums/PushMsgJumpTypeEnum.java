17e1a56c9e786b0a4a31bfbd6325a4dca8fe9dc8
bwang <burgleaf@163.com>
2018-09-06 18:26:03 +0800

    添加app推送路由逻辑

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/PushMsgJumpTypeEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/PushMsgJumpTypeEnum.java
@@ -2,16 +2,30 @@ package com.fulu.game.common.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 @Getter
 @AllArgsConstructor
 public enum PushMsgJumpTypeEnum implements TypeEnum<Integer> {
     H5(1, "H5"),
     PAGE(2, "内部跳转页(小程序）"),
-    OFFCIAL_NOTE(3, "官方公告"),
+    OFFICIAL_NOTICE(3, "官方公告"),
     CHAT_ROOM(4, "聊天室"),
-    CARD_PAGE(5, "名片页");
+    CARD_PAGE(5, "名片页"),
+    INDEX_PAGE(6, "APP首页");
 
     private Integer type;
     private String msg;
+
+
+    public static PushMsgJumpTypeEnum convert(Integer jumpType){
+         for(PushMsgJumpTypeEnum jumpTypeEnum : PushMsgJumpTypeEnum.values()){
+             if(jumpTypeEnum.getType().equals(jumpType)){
+                 return jumpTypeEnum;
+             }
+         }
+         return INDEX_PAGE;
+    }
+
+
 }
