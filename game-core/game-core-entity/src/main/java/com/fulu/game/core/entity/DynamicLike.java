8b50f153bc803cfc0d1a4a52949156ce9b134940
ancyshi <ancyshi@163.com>
2018-09-14 13:36:48 +0800

    动态_添加点赞用户性别

--- game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/DynamicLike.java
+++ game-core/game-core-entity/src/main/java/com/fulu/game/core/entity/DynamicLike.java
@@ -27,6 +27,8 @@ public class DynamicLike implements Serializable {
 	private String fromUserHeadUrl;
 	//点赞用户昵称（冗余字段，提高查询效率）
 	private String fromUserNickname;
+	//点赞用户性别（0：未公开；1：男；2：女）
+	private Integer fromUserGender;
 	//创建时间
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date createTime;
