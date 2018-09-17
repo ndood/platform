9315a6c497847c45ea7ec8ce21df77ce77565af3
bwang <burgleaf@163.com>
2018-09-17 15:50:40 +0800

    添加payment service

--- game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/payment/PaymentServiceImpl.java
+++ game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/payment/PaymentServiceImpl.java
@@ -3,12 +3,15 @@ package com.fulu.game.core.service.impl.payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @Slf4j
 public class PaymentServiceImpl implements PaymentService{
 
 
-
-
-
+    @Override
+    public Object createPayRequest(BigDecimal actualMoney, String orderNo) {
+        return null;
+    }
 }
