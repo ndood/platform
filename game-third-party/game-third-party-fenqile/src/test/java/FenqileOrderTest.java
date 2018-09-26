e5575eb309524117680802d6022d5993455cb15b
bwang <burgleaf@163.com>
2018-09-26 15:12:53 +0800

    添加fenqile test

--- game-third-party/game-third-party-fenqile/src/test/java/FenqileOrderTest.java
+++ game-third-party/game-third-party-fenqile/src/test/java/FenqileOrderTest.java
@@ -1,11 +1,7 @@
-import cn.hutool.core.date.DateUtil;
 import com.fulu.game.thirdparty.fenqile.entity.FenqileConfig;
-import com.fulu.game.thirdparty.fenqile.entity.FenqileOrderRequest;
 import com.fulu.game.thirdparty.fenqile.service.impl.FenqileOrderServiceImpl;
-import com.fulu.game.thirdparty.fenqile.util.SignUtil;
+import org.junit.Test;
 
-import java.math.BigDecimal;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +9,7 @@ public class FenqileOrderTest {
 
 
     public static void main(String[] args) {
-        //timestamp=1514446696&partner_id=PAI201605240000011&format=json&v=1.4&method=fenqile.third.notice.modify
+//        timestamp=1514446696&partner_id=PAI201605240000011&format=json&v=1.4&method=fenqile.third.notice.modify
 //        String partner_key = "f5d764053cd418601131c30da134428b";
 //        Map<String,Object> map = new HashMap<>();
 //        map.put("timestamp",1462533027);
@@ -22,7 +18,7 @@ public class FenqileOrderTest {
 //        map.put("v","1.3");
 //        map.put("method","fenqile.trade.detail.get");
 //        map.put("order_id","O20160113003075100345");
-//
+
 //
 //        String sign = SignUtil.createSign(map,partner_key);
 //        System.out.println(sign);
@@ -38,8 +34,9 @@ public class FenqileOrderTest {
         fenqileConfig.setPartnerKey("43ea4b7f27e36f2eb7b194b3924291ca");
         fenqileConfig.setV("1.4");
         orderService.setConfig(fenqileConfig);
-        Map map =    orderService.noticeModify(1,"http://www.baidu.com",HashMap.class);
+        Map map = orderService.noticeModify(1, "http://www.baidu.com", HashMap.class);
         System.out.println(map);
     }
+
 
 }
