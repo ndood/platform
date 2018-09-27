949cb3dba0272c9477725c766a993bbe2b61af5e
Gong Zechun <gongzechun@fulu.com>
2018-09-27 18:12:55 +0800

    [feature]分期乐：增加发送手机验证码接口

--- game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/fenqile/FenqileHomeController.java
+++ game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/fenqile/FenqileHomeController.java
@@ -1,16 +1,20 @@
 package com.fulu.game.h5.controller.fenqile;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.fulu.game.common.Constant;
 import com.fulu.game.common.Result;
+import com.fulu.game.common.enums.RedisKeyEnum;
 import com.fulu.game.common.exception.LoginException;
 import com.fulu.game.common.exception.ParamsException;
 import com.fulu.game.common.exception.UserException;
+import com.fulu.game.common.utils.SMSUtil;
 import com.fulu.game.common.utils.SubjectUtil;
 import com.fulu.game.core.entity.Banner;
 import com.fulu.game.core.entity.User;
 import com.fulu.game.core.entity.vo.BannerVO;
 import com.fulu.game.core.service.BannerService;
 import com.fulu.game.core.service.UserService;
+import com.fulu.game.core.service.impl.RedisOpenServiceImpl;
 import com.fulu.game.h5.controller.BaseController;
 import com.fulu.game.h5.shiro.PlayUserToken;
 import com.fulu.game.h5.utils.RequestUtil;
@@ -51,6 +55,9 @@ public class FenqileHomeController extends BaseController {
 
     private final FenqileSdkOrderService fenqileSdkOrderService;
 
+    @Autowired
+    private RedisOpenServiceImpl redisOpenService;
+
 
     @Autowired
     public FenqileHomeController(BannerService bannerService,
@@ -66,6 +73,7 @@ public class FenqileHomeController extends BaseController {
 
     /**
      * banner展示列表
+     *
      * @return 封装结果集
      */
     @PostMapping("/banner/list")
@@ -85,28 +93,28 @@ public class FenqileHomeController extends BaseController {
         if (StringUtils.isBlank(code)) {
             throw new ParamsException(ParamsException.ExceptionCode.PARAM_NULL_EXCEPTION);
         }
-        log.info("分期乐code:{}",code);
+        log.info("分期乐code:{}", code);
         CodeSessionResult session = null;
-        if(code.length()<=6){
+        if (code.length() <= 6) {
             session = new CodeSessionResult();
             session.setUid(code);
             session.setAccessToken("tempaccesstoken");
             log.info("使用假的分期乐code登录");
-        }else{
+        } else {
             try {
                 session = fenqileAuthService.accessToken(code);
-                log.info("分期乐授权成功session:{}",session);
+                log.info("分期乐授权成功session:{}", session);
 
-            }catch (Exception e){
-                log.error("分期乐授权错误",e);
+            } catch (Exception e) {
+                log.error("分期乐授权错误", e);
                 throw new LoginException(LoginException.ExceptionCode.FENQILE_AUTH_ERROR);
             }
         }
         //1.认证和凭据的token
         PlayUserToken playUserToken = PlayUserToken.newBuilder(PlayUserToken.Platform.FENQILE)
-                                                        .fqlOpenid(session.getUid())
-                                                        .accessToken(session.getAccessToken())
-                                                        .build();
+                .fqlOpenid(session.getUid())
+                .accessToken(session.getAccessToken())
+                .build();
 
         String ip = RequestUtil.getIpAdrress(request);
         playUserToken.setHost(ip);
@@ -121,16 +129,15 @@ public class FenqileHomeController extends BaseController {
             result.put("token", SubjectUtil.getToken());
             result.put("userId", user.getId());
             return Result.success().data(result).msg("登录成功!");
-        }
-        catch (AuthenticationException e) {
-            if(e.getCause() instanceof UserException){
-                if(UserException.ExceptionCode.USER_BANNED_EXCEPTION.equals(((UserException) e.getCause()).getExceptionCode())){
+        } catch (AuthenticationException e) {
+            if (e.getCause() instanceof UserException) {
+                if (UserException.ExceptionCode.USER_BANNED_EXCEPTION.equals(((UserException) e.getCause()).getExceptionCode())) {
                     log.error("用户被封禁,openId:{}", session);
                     return Result.userBanned();
                 }
             }
             return Result.noLogin();
-        }  catch (Exception e) {
+        } catch (Exception e) {
             log.error("登录异常!", e);
             return Result.error().msg("登陆异常！");
         }
@@ -170,14 +177,44 @@ public class FenqileHomeController extends BaseController {
 
     /**
      * 更新分期乐回调url
+     *
      * @return
      */
     @GetMapping(value = "url/setting")
-    public Result settingUrl(){
+    public Result settingUrl() {
         log.info("执行分期乐修改订单回调通知接口");
         fenqileSdkOrderService.modifyPlatformUrl();
         return Result.success();
     }
 
-
+    /**
+     * 点击发送验证码接口
+     *
+     * @param mobile
+     * @return
+     */
+    @PostMapping("/sms/verify")
+    @ResponseBody
+    public Result sms(@RequestParam("mobile") String mobile) {
+        //缓存中查找该手机是否有验证码
+        if (redisOpenService.hasKey(RedisKeyEnum.SMS.generateKey(mobile))) {
+            String times = redisOpenService.get(RedisKeyEnum.SMS.generateKey(mobile));
+            if (Integer.parseInt(times) > Constant.MOBILE_CODE_SEND_TIMES) {
+                return Result.error().msg("半小时内发送次数不能超过" + Constant.MOBILE_CODE_SEND_TIMES + "次，请等待！");
+            } else {
+                String verifyCode = SMSUtil.sendVerificationCode(mobile);
+                log.info("发送验证码{}={}", mobile, verifyCode);
+                redisOpenService.set(RedisKeyEnum.SMS_VERIFY_CODE.generateKey(mobile), verifyCode, Constant.VERIFYCODE_CACHE_TIME);
+                times = String.valueOf(Integer.parseInt(times) + 1);
+                redisOpenService.set(RedisKeyEnum.SMS_VERIFY_CODE_TIMES.generateKey(mobile), times, Constant.MOBILE_CACHE_TIME);
+                return Result.success().data(verifyCode).msg("验证码发送成功！");
+            }
+        } else {
+            String verifyCode = SMSUtil.sendVerificationCode(mobile);
+            log.info("发送验证码{}={}", mobile, verifyCode);
+            redisOpenService.set(RedisKeyEnum.SMS_VERIFY_CODE.generateKey(mobile), verifyCode, Constant.VERIFYCODE_CACHE_TIME);
+            redisOpenService.set(RedisKeyEnum.SMS_VERIFY_CODE_TIMES.generateKey(mobile), "1", Constant.MOBILE_CACHE_TIME);
+            return Result.success().data(verifyCode).msg("验证码发送成功！");
+        }
+    }
 }
