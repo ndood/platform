9751df251e05b46d9dd743b14523d4bf7029c515
Gong Zechun <gongzechun@fulu.com>
2018-09-29 18:06:29 +0800

    [bug-fix]分期乐用户缓存问题。

--- game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/UserController.java
+++ game-h5/game-h5-controller/src/main/java/com/fulu/game/h5/controller/UserController.java
@@ -123,10 +123,10 @@ public class UserController extends BaseController {
 
             user.setDelFlag(true);
             userService.update(user);
-            userService.updateRedisUser(user);
             log.info("同步用户信息中。逻辑删除userId为：{}的用户", user.getId());
             user = userService.findById(mobileUser.getId());
             log.info("用户同步操作完成。目前登录用户useId：{}", user.getId());
+            userService.updateRedisUser(user);
         } else {
             user.setMobile(mobile);
             user.setUpdateTime(new Date());
