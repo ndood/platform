9086dceb78a6294ab2f25edd4e985648e7c86dcb
Gong Zechun <gongzechun@fulu.com>
2018-09-29 16:33:02 +0800

    [bug-fix]主图修改

--- game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/UserInfoAuthServiceImpl.java
+++ game-core/game-core-service-impl/src/main/java/com/fulu/game/core/service/impl/UserInfoAuthServiceImpl.java
@@ -120,7 +120,7 @@ public class UserInfoAuthServiceImpl extends AbsCommonService<UserInfoAuth, Inte
         UserInfoAuth userInfoAuth = new UserInfoAuth();
         BeanUtil.copyProperties(userInfoAuthTO, userInfoAuth);
         userInfoAuth.setUpdateTime(new Date());
-        userInfoAuthDao.update(userInfoAuth);
+//        userInfoAuthDao.update(userInfoAuth);
 
         //主图不存userInfoAuth，改为存入临时表
         userInfoAuth.setMainPicUrl(null);
