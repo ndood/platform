665da2eea12b0f30aad569654884d4b5ead3a708
bwang <burgleaf@163.com>
2018-10-09 15:04:49 +0800

    角色管理功能

--- game-core/game-core-common/src/main/java/com/fulu/game/common/enums/RoomRoleTypeEnum.java
+++ game-core/game-core-common/src/main/java/com/fulu/game/common/enums/RoomRoleTypeEnum.java
@@ -1,4 +1,17 @@
 package com.fulu.game.common.enums;
 
-public class RoomRoleTypeEnum {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum  RoomRoleTypeEnum implements TypeEnum<Integer>{
+
+
+    owner(1, "手机号"),
+    manage(2, "QQ号"),
+    preside(3, "微信号");
+
+    private Integer type;
+    private String msg;
 }
